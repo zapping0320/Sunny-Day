cfbdffa057db1d7cfb3b57500f278f6a7a3c036e
John DongHyun Kim <zapping0320@gmail.com>
2018-12-07 07:40:23 +0900

    지역 범위를 벗어난 것에 대한 예외 메시지 추가

--- YJWeather/Model/Request/RequestError.swift
+++ YJWeather/Model/Request/RequestError.swift
@@ -12,6 +12,7 @@ enum RequestError: Error {
     case networkConnection
     case networkDelay
     case requestFailed
+    case outOfValidLocation
 }
 
 extension RequestError: LocalizedError {
@@ -23,6 +24,8 @@ extension RequestError: LocalizedError {
             return NSLocalizedString("네트워크 연결이 지연되고 있습니다. 잠시 후에 다시 시도해주세요.", comment: "RequestError")
         case .requestFailed:
             return NSLocalizedString("요청이 실패하였습니다. 잠시 후에 다시 시도해주세요.", comment: "RequestError")
+        case .outOfValidLocation:
+            return NSLocalizedString("앱 실행에 유효한 지역이 아닙니다.", comment: "RequestError")
         }
     }
 }
